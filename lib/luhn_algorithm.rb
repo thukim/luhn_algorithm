9f3bea51ce7c552b895e0f428317bd2359ad6fc1
Kimmie <kimthu.bui@gmail.com>
2019-07-06 22:48:35 +0700

    Add method to check if a value is valid based on Luhn Algorithm

--- lib/luhn_algorithm.rb
+++ lib/luhn_algorithm.rb
@@ -1,6 +1,15 @@
 require 'luhn_algorithm/version'
 
 module LuhnAlgorithm
-  class Error < StandardError; end
-  # Your code goes here...
+  NUMBER_ONLY = /^\d+$/
+
+  def self.valid?(value)
+    value = value.to_s.reverse
+    return false unless value.match(NUMBER_ONLY)
+    arr = value.chars.map(&:to_i)
+    (1...(arr.length)).step(2) do |index|
+      arr[index] = (arr[index] * 2).divmod(10).sum
+    end
+    (arr.sum % 10).zero?
+  end
 end
--- spec/luhn_algorithm_spec.rb
+++ spec/luhn_algorithm_spec.rb
@@ -3,7 +3,54 @@ RSpec.describe LuhnAlgorithm do
     expect(LuhnAlgorithm::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  [
+    {
+      value: '6011111111111117',
+      result: true
+    },
+    {
+      value: '6011111111111118',
+      result: false
+    },
+    {
+      value: 4222222222222,
+      result: true
+    },
+    {
+      value: '4222221222222',
+      result: false
+    },
+    {
+      value: '4522222222222',
+      result: false
+    },
+    {
+      value: '4522222222222',
+      result: false
+    },
+    {
+      value: '45222222222220',
+      result: false
+    },
+    {
+      value: '45222222222220',
+      result: false
+    },
+    {
+      value: '4111111111111111',
+      result: true
+    },
+    {
+      value: 'A122333333333',
+      result: false
+    },
+    {
+      value: 6011000990139424,
+      result: true
+    }
+  ].each do |hs|
+    it "return #{hs[:result]} for value #{hs[:value]}" do
+      expect(LuhnAlgorithm.valid?(hs[:value])).to eq(hs[:result])
+    end
   end
 end
